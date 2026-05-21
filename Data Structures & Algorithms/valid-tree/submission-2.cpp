470fd19d245beb900d4471cadab8c38ac50d2f29
kaushalrokaya11-cloud <kaushalrokaya11@gmail.com>
2026-05-21 14:39:33 +0545

    Add: valid-tree - submission-2

---
+++ Data Structures & Algorithms/valid-tree/submission-2.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool validTree(int n, vector<vector<int>>& edges) {
+        vector<vector<int>> adj(n);
+        queue<pair<int,int>> q;
+        unordered_set<int> curr;
+
+        for(vector<int> edge:edges){
+            adj[edge[0]].push_back(edge[1]);
+            adj[edge[1]].push_back(edge[0]);
+        }
+
+            q.push({0,-1});
+            curr.insert(0);
+            return bfs(adj,q,curr,n);
+
+        
+    }
+    bool bfs(vector<vector<int>> &adj,queue<pair<int,int>> &q, unordered_set<int> &curr, int n){
+        // int total=1;
+        while(!q.empty()){
+            auto [node,parent]= q.front();
+            q.pop();
+            for(auto nd: adj[node]){
+                if(nd==parent){
+                    continue;
+                }
+                if(curr.count(nd)){return false;}
+                q.push({nd,node});
+                curr.insert(nd);
+            
+
+            }
+        }
+        return (curr.size()==n);
+    }
+};
