fe8bdbccae4a6eb81b0acb97592b8f320829a3e8
kaushalrokaya11-cloud <kaushalrokaya11@gmail.com>
2026-05-24 13:19:33 +0545

    Add: kth-largest-integer-in-a-stream - submission-0

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-0.cpp
@@ -0,0 +1,27 @@
+class KthLargest {
+public:
+int k;
+vector<int> nums;
+priority_queue<int,vector<int>, greater<int>> heap;
+
+    KthLargest(int k, vector<int>& nums) {
+        this->k =k;
+
+        for(int num:nums){
+            heap.push(num);
+            if(heap.size()>k){
+                heap.pop();
+            }
+        }
+        
+    }
+    
+    int add(int val) {
+
+        heap.push(val);
+        if(heap.size()>k){heap.pop();}
+        if(heap.size()<k) return -1;
+        return heap.top();
+        
+    }
+};
