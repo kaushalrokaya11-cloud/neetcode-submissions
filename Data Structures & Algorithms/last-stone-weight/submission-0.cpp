86f6a2cc6bb126be9e9f221ffd491827a310a1d1
kaushalrokaya11-cloud <kaushalrokaya11@gmail.com>
2026-05-24 14:11:27 +0545

    Add: last-stone-weight - submission-0

---
+++ Data Structures & Algorithms/last-stone-weight/submission-0.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> maxHeap;
+
+        for(int stone: stones){
+            maxHeap.push(stone);
+            
+        }
+
+        while(maxHeap.size()> 1){
+          int f= maxHeap.top();
+          maxHeap.pop();
+          int s= maxHeap.top();
+          maxHeap.pop();
+
+          if(f==s){continue;}
+
+          else if(f>s){maxHeap.push(f-s);}
+          else{ maxHeap.push(s-f);}
+
+        }
+       return (maxHeap.size()==1?maxHeap.top():0);
+        
+    }
+};
