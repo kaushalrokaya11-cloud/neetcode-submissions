d925b2485c7fa0f268af7f8c8c5762e40b2dd3e1
kaushalrokaya11-cloud <kaushalrokaya11@gmail.com>
2026-05-31 15:25:20 +0545

    Add: jump-game - submission-0

---
+++ Data Structures & Algorithms/jump-game/submission-0.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        if(nums.size()==0)return true;
+        int checkpoint = nums.size()-1;
+
+        for(int i=nums.size()-1;i>=0;i--){
+            if((nums[i]+i)>=checkpoint){ checkpoint =i; cout<<
+             "The newCheckpoint is "<< checkpoint<<endl;} 
+            
+        }
+        if(checkpoint==0)return true;
+        else return false;
+    }
+};
